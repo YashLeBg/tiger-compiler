f9d5a5f93384b9f9f49bbff836b3ac3edaa66184
colin.gomez <colin.gomez@epita.fr>
2026-03-12 18:14:53 +0100

    feat: implement object visitor

--- src/ast/object-visitor.hxx
+++ src/ast/object-visitor.hxx
@@ -23,35 +23,43 @@ namespace ast
   /*-------------------------------.
   | Object-related visit methods.  |
   `-------------------------------*/
-
+  //visite la classe parent ses champs et ses methodes
   template <template <typename> class Const>
   void GenObjectVisitor<Const>::operator()(const_t<ClassTy>& e)
   {
-    // FIXME: Some code was deleted here.
+    this->accept(e.super_get());
+    e.fields_get().accept(*this);
+    e.methods_get().accept(*this);
   }
-
+  //visite les declarations de methode dans le chunk
   template <template <typename> class Const>
   void GenObjectVisitor<Const>::operator()(const_t<MethodChunk>& e)
   {
-    // FIXME: Some code was deleted here.
+    e.formals_get().accept(*this);
+    this->accept(e.result_get());
+    this->accept(e.body_get());
   }
-
+  //visite les parametres le type de retour et le body de la methode
   template <template <typename> class Const>
   void GenObjectVisitor<Const>::operator()(const_t<MethodDec>& e)
   {
-    // FIXME: Some code was deleted here.
+    e.object_get().accept(*this);
+    for (auto* arg : e.args_get())
+      arg->accept(*this);
   }
-
+  //visite l objet et les arguments de l appel de methode
   template <template <typename> class Const>
   void GenObjectVisitor<Const>::operator()(const_t<MethodCallExp>& e)
   {
-    // FIXME: Some code was deleted here.
+    e.class_name_get().accept(*this);
+    for (auto* arg : e.args_get())
+      arg->accept(*this);
   }
-
+  //visite le nom de classe et les arguments du nouvel objet
   template <template <typename> class Const>
   void GenObjectVisitor<Const>::operator()(const_t<ObjectExp>& e)
   {
-    // FIXME: Some code was deleted here.
+    e.object_get().accept(*this);
   }
 
 } // namespace ast
