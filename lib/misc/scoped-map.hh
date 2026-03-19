948aba80d13b38609b515c90595b9c77338cf00e
jessim-ziani <zianijessim@gmail.com>
2026-03-19 23:16:33 +0100

    feat:merge scoped_map

--- lib/misc/scoped-map.hh
+++ lib/misc/scoped-map.hh
@@ -14,19 +14,41 @@
 
 #include <map>
 #include <vector>
+#include <type_traits>
 
 namespace misc
 {
   template <typename Key, typename Data> class scoped_map
   {
-    // FIXME: Some code was deleted here.
+    // FIXED: Some code was deleted here.
+    public:
+    scoped_map();
+    ~scoped_map() = default;
+    // Associated value to key in the current scope
+    void put(const Key& key, const Data& value);
+    // If key was associated to some Data in the open scoped, return the most recent insertion.
+    // Otherwise, if Data is a pointer type, then return the null pointer,
+    // else throw a std::range_error
+    Data get(const Key& key) const;
+    // Send the content of this table on ostr in a human-readable manner, and return the stream
+    std::ostream& dump(std::ostream& ostr) const;
+    // Open a new scope
+    void scope_begin();
+    // Close the last scope, forgetting everything since the latest scope_begin()
+    void scope_end();
+    private:
+    std::vector<std::map<Key, Data>> scope_;
   };
 
   template <typename Key, typename Data>
   std::ostream& operator<<(std::ostream& ostr,
                            const scoped_map<Key, Data>& tbl);
 
-  // FIXME: Some code was deleted here.
+  // FIXED: Some code was deleted here.
+  // creation of concept to check if T is a pointer
+  template <typename T>
+  concept CheckPointer= std::is_pointer_v<T>;
+
 
 } // namespace misc
 
--- lib/misc/scoped-map.hxx
+++ lib/misc/scoped-map.hxx
@@ -13,9 +13,78 @@
 #include <misc/contract.hh>
 #include <misc/indent.hh>
 
+#include "scoped-map.hh"
+
 namespace misc
 {
-  // FIXME: Some code was deleted here.
+  // FIXED: Some code was deleted here.
+  template <typename Key, typename Data>
+  scoped_map<Key, Data>::scoped_map()
+  {
+      //create the first scope
+      scope_.push_back(std::map<Key, Data>());
+  }
+
+  template<typename Key, typename Data>
+  void scoped_map<Key, Data>::scope_begin() {
+      // put a new map for a new scope at the top of stack
+      scope_.push_back(std::map<Key, Data>());
+  }
+
+  template<typename Key, typename Data>
+  void scoped_map<Key, Data>::scope_end() {
+      if (!scope_.empty()) {
+          // delete the most recent map and forget all vars in it
+          scope_.pop_back();
+      }
+  }
+
+  template<typename Key, typename Data>
+  void scoped_map<Key, Data>::put(const Key &key, const Data &value) {
+      // add key and value to the current scope
+      scope_.back()[key] = value;
+  }
+
+  template<typename Key, typename Data>
+  Data scoped_map<Key, Data>::get(const Key &key) const {
+      auto e = scope_.end();
+      // go throught all scope starting by the most recent scope
+      while (e != scope_.begin()) {
+          e--;
+          // looking for the key in this scope
+          auto find = e->find(key);
+          // if we found it return his data
+          if (find != e->end()) {
+              return find->second;
+          }
+      }
+      // we didn't find the key
+      // check if Data is a pointer
+      if constexpr (CheckPointer<Data>)
+      {
+          return nullptr;
+      }
+      else {
+          throw std::range_error("not found");
+      }
+
+  }
+
+  template<typename Key, typename Data>
+  std::ostream &scoped_map<Key, Data>::dump(std::ostream &ostr) const {
+      // counter of scope
+      int scope_cpt = 0;
+      // go throught all scope beginning by the global one to the current one
+      for (auto i = scope_.begin(); i != scope_.end();i++) {
+          ostr << "------- Scope " << scope_cpt << " -------\n";
+          // foreach scope print pair key-value
+          for (auto j = i->begin(); j != i->end();j++) {
+              ostr << j->first << ": " << j->second << "\n";
+          }
+          scope_cpt++;
+      }
+      return ostr;
+  }
 
   template <typename Key, typename Data>
   inline std::ostream& operator<<(std::ostream& ostr,
