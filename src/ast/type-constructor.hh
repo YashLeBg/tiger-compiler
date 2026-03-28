660cf45c2db9b76b969987046184c0c046526a73
Louis <louis-maximilien.chappuit@epita.fr>
2026-03-28 11:11:49 +0100

    feat:typable

--- src/ast/type-constructor.hh
+++ src/ast/type-constructor.hh
@@ -7,19 +7,20 @@
 
 #include <ast/fwd.hh>
 #include <type/fwd.hh>
-
+#include <ast/typable.hh>
 namespace ast
 {
   /** \class ast::TypeConstructor
    ** \brief Create a new type.
    */
 
-  class TypeConstructor
+  class TypeConstructor:public Typable
   {
     // FIXME: Some code was deleted here.
     // basicly thats the same thing as typebale but there we juste stock the created type
-    TypeConstructor();
-    virtual ~TypeConstructor()=default;
+      public:
+          TypeConstructor();
+    virtual ~TypeConstructor(){delete created_type_;}
     const type::Type* created_type_get() const;
     void created_type_set(const type::Type* t);
     protected:
