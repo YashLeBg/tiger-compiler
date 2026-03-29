0d8cee729559ca4c44d589c9c70a2f032c11a234
jessim-ziani <zianijessim@gmail.com>
2026-03-29 17:14:38 +0200

    feat:typecheck complex nodes

--- src/type/type-checker.cc
+++ src/type/type-checker.cc
@@ -3,6 +3,8 @@
  ** \brief Implementation for type/type-checker.hh.
  */
 
+#include "type-checker.hh"
+
 #include <memory>
 #include <ranges>
 
@@ -20,12 +22,18 @@ namespace type
   const Type* TypeChecker::type(ast::Typable& e)
   {
     // FIXME: Some code was deleted here.
+    e.accept(*this);
+    return e.type_get();
   }
 
   const Record* TypeChecker::type(const ast::fields_type& e)
   {
     auto res = new Record;
     // FIXME: Some code was deleted here.
+    for (const auto& f:e) {
+      f->type_name_get().accept(*this);
+      res->field_add(f->name_get(),*f->type_name_get().type_get());
+    }
     return res;
   }
 
@@ -68,6 +76,9 @@ namespace type
                                 const Type& type2)
   {
     // FIXME: Some code was deleted here (Check for type mismatch).
+    if (!type1.compatible_with(type2)) {
+      type_mismatch(ast, exp1, type1, exp2, type2);
+    }
   }
 
   void TypeChecker::check_types(const ast::Ast& ast,
@@ -80,6 +91,7 @@ namespace type
     type(type1);
     type(type2);
     // FIXME: Some code was deleted here (Check types).
+    check_types(ast,exp1,*type(type1),exp2, *type(type2));
   }
 
   /*--------------------------.
@@ -122,6 +134,24 @@ namespace type
   void TypeChecker::operator()(ast::RecordExp& e)
   {
     // FIXME: Some code was deleted here.
+    e.type_name_get().accept(*this);
+    const auto* t = &e.type_name_get().type_get()->actual();
+    const auto* record = dynamic_cast<const Record*>(t);
+    if (!record)
+    {
+      error(e, "not a record type", *e.type_name_get().name_get());
+      type_set(e, &Void::instance());
+      return;
+    }
+    for (const auto& field : e.fields_get())
+    {
+      const Type* exp = record->field_type(field->name_get());
+      if (!exp)
+        error(*field, "unexpected field", field->name_get());
+      else
+        check_types(field->init_get(), "field", *this->type(field->init_get()), "expected", *exp);
+    }
+    type_set(e, record);
   }
 
   void TypeChecker::operator()(ast::OpExp& e)
@@ -130,6 +160,68 @@ namespace type
   }
 
   // FIXME: Some code was deleted here.
+  void TypeChecker::operator()(ast::LetExp& e) {
+    venv_.scope_begin();
+    tenv_.scope_begin();
+    e.chunks_get().accept(*this);
+    type_set(e, type(e.body_get()));
+    tenv_.scope_end();
+
+    venv_.scope_end();
+  }
+
+  void TypeChecker::operator()(ast::ArrayExp& e)
+  {
+    e.type_name_get().accept(*this);
+    const Type* res = &e.type_name_get().type_get()->actual();
+    const Array* arr = dynamic_cast<const Array*>(res);
+    if (!arr)
+    {
+      error(e, "not an array type", *e.type_name_get().name_get());
+      type_set(e, &Void::instance());
+      return;
+    }
+    check_type(e.size_get(), "size", Int::instance());
+    check_types(e.init_get(), "init", *type(e.init_get()),"type", arr->type_get());
+    type_set(e, arr);
+  }
+
+  void TypeChecker::operator()(ast::CallExp& e)
+  {
+    const auto* f = dynamic_cast<const Function*>(venv_.look(e.name_get()));
+    if (!f)
+    {
+      error(e, "undefined function", e.name_get());
+      type_set(e, &Void::instance());
+      return;
+    }
+    const auto& form = f->formals_get();
+    const auto& argv = e.args_get();
+    auto it = form.begin();
+    auto arg_it = argv.begin();
+    for (int i = 0; it != form.end() && arg_it != argv.end(); ++it, ++arg_it) {
+      check_types(**arg_it, "arg", *type(**arg_it), "formal", it->type_get());
+      i++;
+    }
+    if (it != form.end() || arg_it != argv.end()) {
+      error(e, "argument count mismatch");
+    }
+    type_set(e, &f->result_get());
+  }
+
+  void TypeChecker::operator()(ast::ForExp& e)
+  {
+    check_type(e.vardec_get().init_get(), "lower bound", Int::instance());
+
+    check_type(e.hi_get(), "born upper", Int::instance());
+    venv_.scope_begin();
+
+    e.vardec_get().accept(*this);
+    var_read_only_.insert(&e.vardec_get());
+    type(e.body_get());
+    type_set(e, &Void::instance());
+    venv_.scope_end();
+  }
 
   /*-----------------.
   | Visiting /Dec/.  |
@@ -155,6 +247,17 @@ namespace type
   void TypeChecker::visit_dec_header<ast::FunctionDec>(ast::FunctionDec& e)
   {
     // FIXME: Some code was deleted here.
+    const Type* res = &Void::instance();
+    if (e.result_get())
+    {
+      e.result_get()->accept(*this);
+      res = e.result_get()->type_get();
+    }
+    const Record* f = type(e.formals_get());
+    auto func = new Function(f,*res);
+
+    type_set(e,func);
+    venv_.push(e.name_get(),func);
   }
 
   // Type check this function's body.
@@ -172,6 +275,27 @@ namespace type
   void TypeChecker::operator()(ast::VarDec& e)
   {
     // FIXME: Some code was deleted here.
+    const Type* init = nullptr;
+    if (e.init_get()) {
+      init = type(*e.init_get());
+    }
+    const Type* t = nullptr;
+    if (e.type_name_get()) {
+      e.type_name_get()->accept(*this);
+      t = e.type_name_get()->type_get();
+    }
+    if (t && init) {
+      check_types(e, "init", *init, "type", *t);
+    }
+    if (!t && init && init->actual() == Nil::instance()) {
+      error(e,"can't use nil where its type can't be determinee");
+    }
+    const Type* type = init;
+    if (t) {
+      type = t;
+    }
+    type_set(e,type);
+    venv_.push(e.name_get(),type);
   }
 
   /*--------------------.
@@ -195,13 +319,23 @@ namespace type
     // We only process the head of the type declaration, to set its
     // name in E.  A declaration has no type in itself; here we store
     // the type declared by E.
-    // FIXME: Some code was deleted here.
+    // FIXED: Some code was deleted here.
+    auto name = new Named(e.name_get());
+    tenv_.push(e.name_get(),name);
+    type_set(e,name);
   }
 
   // Bind the type body to its name.
   template <> void TypeChecker::visit_dec_body<ast::TypeDec>(ast::TypeDec& e)
   {
     // FIXME: Some code was deleted here.
+    const Type* t = tenv_.look(e.name_get());
+    const Named* name = dynamic_cast<const Named*>(t);
+    e.ty_get().accept(*this);
+    name->type_set(e.ty_get().type_get());
+    if (!name->sound()) {
+      error(e, "rec def", e.name_get());
+    }
   }
 
   /*------------------.
@@ -210,7 +344,13 @@ namespace type
 
   template <class D> void TypeChecker::chunk_visit(ast::Chunk<D>& e)
   {
-    // FIXME: Some code was deleted here.
+    // FIXED: Some code was deleted here.
+    for (auto& a : e) {
+      visit_dec_header(*a);
+    }
+    for (auto& a : e) {
+      visit_dec_body(*a);
+    }
   }
 
   /*-------------.
--- src/type/type-checker.hxx
+++ src/type/type-checker.hxx
@@ -20,19 +20,26 @@ namespace type
   template <typename NodeType>
   void TypeChecker::type_default(NodeType& e, const type::Type* type)
   {
-    // FIXME: Some code was deleted here.
+    // FIXED: Some code was deleted here.
+    if (!e.type_get()) {
+      type_set(e,type);
+    }
   }
 
   template <typename NodeType>
   void TypeChecker::created_type_default(NodeType& e, const type::Type* type)
   {
-    // FIXME: Some code was deleted here.
+    // FIXED: Some code was deleted here.
+    if (!e.type_get()) {
+      type_set(e, type);
+    }
   }
 
   template <typename NodeType>
   void TypeChecker::type_set(NodeType& e, const type::Type* type)
   {
-    // FIXME: Some code was deleted here (Basically e.type_set(type)).
+    // FIXED: Some code was deleted here (Basically e.type_set(type)).
+    e.type_set(type);
   }
 
   /*-----------------.
@@ -55,6 +62,7 @@ namespace type
 
     const type::Type* nil_error_ = nullptr;
     // FIXME: Some code was deleted here (Get the Nil type).
+    nil_error_ = &Nil::instance();
     loc.type_set(nil_error_);
   }
 
@@ -63,7 +71,11 @@ namespace type
                                const std::string& s,
                                const Type& expected)
   {
-    // FIXME: Some code was deleted here.
+    // FIXED: Some code was deleted here.
+    const Type* cur = type(e);
+    if (!cur->compatible_with(expected)) {
+      type_mismatch(e,s,*cur,"expected",expected);
+    }
   }
 
   /*------------.
@@ -74,6 +86,16 @@ namespace type
   void TypeChecker::visit_routine_body(Routine_Node& e)
   {
     // FIXME: Some code was deleted here.
+    venv_.scope_begin();
+    for (const auto& f:e.formals_get()) {
+      f->accept(*this);
+      venv_.push(f->name_get(),f);
+    }
+    const Type* body = type(*e.body_get());
+    const Type& curr_type = e.type_get()->actual();
+    const Type& res = dynamic_cast<const Routine_Type&>(curr_type).result_get();
+    check_types(*e.body_get(), "result",*body,"expected",res);
+    venv_.scope_end();
   }
 
 } // namespace type
